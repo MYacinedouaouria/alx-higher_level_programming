d9e22708cc3679a11918fd7b71f254caff8a3cd1
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-04 12:44:16 -0800

    fixed pycodestyle error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if (number >= 0):
     last_digit = number % 10
 elif (number < 0):
-    last_digit = (( -1 * number) % 10) * -1
+    last_digit = ((-1 * number) % 10) * -1
 
 if (last_digit == 0):
     print(f"Last digit of {number} is {last_digit} and is 0")
