a9771e18c0979a612dc1545207d43edf3e1234c0
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-06 09:22:19 -0800

    using string format instead of f-strings

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,12 +2,12 @@
 def uppercase(str):
     for i in range(len(str) - 1):
         print(
-            f"{chr(ord(str[i]) - 32)}" if
-            97 <= ord(str[i]) <= 122 else f"{str[i]}",
+            "{}".format(chr(ord(str[i]) - 32)) if
+            97 <= ord(str[i]) <= 122 else "{}".format(str[i]),
             end=""
             )
 
     print(
-        f"{chr(ord(str[-1]) - 32)}" if
-        97 <= ord(str[-1]) <= 122 else f"{str[-1]}"
+        "{}".format(chr(ord(str[-1]) - 32)) if
+        97 <= ord(str[-1]) <= 122 else "{}".format(str[-1])
         )
