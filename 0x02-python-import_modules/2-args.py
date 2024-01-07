a3e4d500773c44de69932f64e5d2c044703d149d
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-07 08:20:07 -0800

    printing 0 arguments. in case no arguments are passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 if (__name__ == "__main__"):
     if (len(sys.argv) == 1):
-        print("{:d} argument.".format(len(sys.argv) - 1))
+        print("{:d} arguments.".format(len(sys.argv) - 1))
     elif (len(sys.argv) == 2):
         print("{:d} argument:".format(len(sys.argv) - 1))
     else:
