0e648d409249532cc2de05b1919dd44e66acb238
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-10 05:01:19 -0800

    fixed the function for case of empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end="")
+    if (matrix == [[]]):
         print()
+    else:
+        for row in matrix:
+            for i in range(len(row) - 1):
+                print("{:d}".format(row[i]), end=" ")
+            print(row[-1])
