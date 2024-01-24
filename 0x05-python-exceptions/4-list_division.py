c9a50830fef01081bd2ca3b8c300bc6c20942808
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-24 11:17:17 -0800

    added finaly in my function

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,4 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             list.append(0)
+        finally:
+            continue
     return list
