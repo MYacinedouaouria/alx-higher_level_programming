44dcddd9f7b1d33e7587bd14bb7313d61bef05d9
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-24 12:15:07 -0800

    added  a check if both element are either int or float

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,10 +3,12 @@ def list_division(my_list_1, my_list_2, list_length):
     list = []
     for i in range(list_length):
         try:
-            list.append(my_list_1[i] / my_list_2[i])
-        except (TypeError, ValueError):
-            print("Wrong type")
-            list.append(0)
+            if (isinstance(my_list_1[i], (int, float)) and
+                    isinstance(my_list_2[i], (int, float))):
+                list.append(my_list_1[i] / my_list_2[i])
+            else:
+                print("Wrong type")
+                list.append(0)
         except ZeroDivisionError:
             print("division by 0")
             list.append(0)
