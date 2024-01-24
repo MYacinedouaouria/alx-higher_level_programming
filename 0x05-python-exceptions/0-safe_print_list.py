0c5b5ba741c3c10e6ce1080be65d824bd9992f10
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-23 16:27:03 -0800

    solved pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-	nb = 0
-	for i in range(x):
-		try:
-			print(f"{my_list[i]:d}", end="")
-			nb += 1
-		except IndexError:
-			break
-	print()
-	return nb
+    nb = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]:d}", end="")
+            nb += 1
+        except IndexError:
+            break
+    print()
+    return nb
