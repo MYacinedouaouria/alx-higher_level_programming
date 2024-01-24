40406ff55e88455589345375552bb90f0b8dd6ad
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-24 11:14:20 -0800

    wrote a function that devides element by element 2 lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    list = []
+    for i in range(list_length):
+        try:
+            list.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+            list.append(0)
+        except (TypeError, ValueError):
+            print("Wrong type")
+            list.append(0)
+        except IndexError:
+            print("out of range")
+            list.append(0)
+    return list
