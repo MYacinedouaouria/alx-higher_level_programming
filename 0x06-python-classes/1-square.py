ff4f2e76d7af0c5d16425ae75509add14b777f3a
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-26 11:54:32 -0800

    wrote a class Square that defines a square by a private instance attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" contain a simple square class """
+
+
+class Square:
+    """ a simple square class """
+
+    def __init__(self, size=0):
+        """ initializes the square """
+        self.__size = size
