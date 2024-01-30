c6a8dcd2017fddeb241e5557d1914a10ee659ee7
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-30 15:59:26 -0800

    created module 5-text_indentation and wrote documentations

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+   this is the module '5-text_indentation'
+   this module suply one function , 'text_indentation'
+   that prints a text with 2 new lines after each of these characters: .? and :
+"""
+
+
+def text_indentation(text):
+    """ prints a text with 2 new lines after each of these characters: .? and :
+        text must be a string, otherwise raise a TypeError exception
+        There should be no space at the beginning or at the end of each printed line"""
