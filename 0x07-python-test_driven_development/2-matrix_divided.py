9720ade6caef2b517d7a79d93f6ba0de39b3baa8
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-30 05:40:58 -0800

    wrote documentation for the module and the function

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+   this is the "2-matrix_divided" module
+   this module suplies on function, matrix_divided()
+   Prototype: def matrix_divided(matrix, div):
+"""
+
+
+def matrix_divided(matrix, div):
+    """ divides all elements of a matrix
+        matrix must be a list of lists of integers or floats
+        each row must be the same size and div must be a number != 0"""
