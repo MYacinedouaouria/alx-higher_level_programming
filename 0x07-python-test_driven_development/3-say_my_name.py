ddc348a8c4663086208035280f6640b372943f25
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-30 10:28:08 -0800

    created and documented the module 3-say_my_name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+   this is the "3-say_my_name" module 
+   this module suplies on function , say_my_name()
+   Prototype: def say_my_name(first_name, last_name=""):
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """  prints My name is <first name> <last name>
+         first_name and last_name must be strings otherwise,
+         raise a TypeError exception with a message """
+
