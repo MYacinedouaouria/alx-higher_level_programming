33c83086e5e2d58273f57b5ea23fa523b4064068
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-30 12:54:36 -0800

    defined function say_my_name that prints My name is <first name> <last name>

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-   this is the "3-say_my_name" module 
+   this is the "3-say_my_name" module
    this module suplies on function , say_my_name()
    Prototype: def say_my_name(first_name, last_name=""):
 """
@@ -11,3 +11,9 @@ def say_my_name(first_name, last_name=""):
          first_name and last_name must be strings otherwise,
          raise a TypeError exception with a message """
 
+    if (isinstance(first_name, str) and isinstance(last_name, str)):
+        print(f"My name is {first_name} {last_name}")
+    elif (not isinstance(first_name, str)):
+        raise TypeError("first_name must be a string")
+    else:
+        raise TypeError("last_name must be a string")
