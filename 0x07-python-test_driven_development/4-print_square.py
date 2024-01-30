95254ea12882249b465003a85c9fa85b2a966fe0
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-30 14:21:08 -0800

    defined the function print_square that prints a square with the caracter #

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,3 +10,15 @@ def print_square(size):
     """prints a square with the character #
        size is the size of the square , and it must be an integer otherwise
        raise a TypeError exception , and >= 0 otherwise raise a ValueError"""
+
+    if (not isinstance(size, int)):
+        raise TypeError("size must be an integer")
+    elif (size < 0):
+        raise ValueError("size must be >= 0")
+    else:
+        if (size == 0):
+            print()
+        for i in range(size):
+            for j in range(size - 1):
+                print("#", end="")
+            print("#")
