ac4a6a163c908b1c7fd8fee9014505508c1ea319
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-30 14:07:54 -0800

    created the module 4-print_square and wrote documentations

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+   this is the '4-print_square' module
+   this module suplies on function , print_square()
+   prototype def print_square(size):
+"""
+
+
+def print_square(size):
+    """prints a square with the character #
+       size is the size of the square , and it must be an integer otherwise
+       raise a TypeError exception , and >= 0 otherwise raise a ValueError"""
