3f1d6b7195d77204f2a5c063221e0c8914c2dda0
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-30 16:24:30 -0800

    defined the function text_indentation that prints a text with 2 new lines after each of these characters: ., ? and :

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,4 +9,18 @@
 def text_indentation(text):
     """ prints a text with 2 new lines after each of these characters: .? and :
         text must be a string, otherwise raise a TypeError exception
-        There should be no space at the beginning or at the end of each printed line"""
+        There should be no space at the beginning or at the end of each line"""
+
+    if (not isinstance(text, str)):
+        raise TypeError("text must be a string")
+    new_line = True
+    for i in range(len(text)):
+        if (text[i] == " " and new_line):
+            continue
+        elif (text[i] in ".?:"):
+            print(text[i])
+            print()
+            new_line = True
+        else:
+            print(text[i], end="")
+            new_line = False
