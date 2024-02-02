402d8fa58f42595c3efa99cd189d8c148434de76
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-02 07:21:57 -0800

    created module 1-rectangle and documented it

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+   this is the 1-rectangle module
+   contain definition of the class Rectangle , a real definition of a rectangle
+
+"""
+
+
+class Rectangle:
+    """ a class representing a Rectangle object
+        Atributes:
+            width: have to be an integer and >= to 0
+            height: have to be an integer and >= to 0
+    """
