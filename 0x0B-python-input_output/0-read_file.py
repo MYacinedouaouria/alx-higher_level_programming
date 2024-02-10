836ecafc07baddc0ac7c077f9db532ff85b318f4
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-10 13:53:35 -0800

    fixed  pycodestyle error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ this function reads a text file and prints it to stdout"""
 
     with open(filename, encoding="utf-8") as a_file:
-        print (a_file.read(), end="")
+        print(a_file.read(), end="")
