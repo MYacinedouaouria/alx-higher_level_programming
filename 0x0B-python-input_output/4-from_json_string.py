2dcbe0bf4f8cd3ff84bdf8eca072aa666c91718a
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-11 05:45:13 -0800

    wrote a function that deserializes a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" this module suplies one function, from_json_string()"""
+
+import json
+
+
+def from_json_string(my_str):
+    """ returns an object (Python data structure) represented by
+        a JSON string"""
+
+    return json.loads(my_str)
