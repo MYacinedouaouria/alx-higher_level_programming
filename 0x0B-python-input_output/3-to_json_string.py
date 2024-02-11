35ca64160ac0c2d05e46fdf5407031315dedaf3c
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-11 05:27:37 -0800

    wrote a function that serializes a python object to json formatted string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" this module sulpies one function, to_json_string()"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ returns the JSON representation of an object"""
+
+    return json.dumps(my_obj)
