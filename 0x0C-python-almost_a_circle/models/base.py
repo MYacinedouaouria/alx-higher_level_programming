3240bb1bfc9ecd66cbd545798677db0e63e213d4
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-12 09:54:34 -0800

    defined the class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,3 +7,18 @@ class Base:
         private class attribute : __nb_objects initialized to 0
         class constructor: def __init__(self, id=None):
     """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initializes a Base instance,
+        if id is not None then we assign the public instance attribute id
+        with this argument value
+        otherwise, we increment __nb_objects and assign the new value to the
+        public instance attribute id"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
