e98740496f09b43e2c3d43350e5988d059355ca2
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-12 13:47:47 -0800

    Added tests to ensure proper validation of attribute values

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -13,3 +13,67 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r2.id, 2)
         self.assertEqual(r3.id, 12)
         self.assertEqual(r4.id, 3)
+
+    def test_invalid_width_type(self):
+        with self.assertRaises(TypeError) as e:
+            Rectangle("w", 2)
+        self.assertEqual(str(e.exception), "Width must be an integer")
+
+        with self.assertRaises(TypeError) as e:
+            Rectangle(None, 2)
+        self.assertEqual(str(e.exception), "Width must be an integer")
+
+    def test_invalid_height_type(self):
+        with self.assertRaises(TypeError) as e:
+            Rectangle(3, "2")
+        self.assertEqual(str(e.exception), "height must be an integer")
+
+        with self.assertRaises(TypeError) as e:
+            Rectangle(2, None)
+        self.assertEqual(str(e.exception), "height must be an integer")
+
+    def test_invalid_x_type(self):
+        with self.assertRaises(TypeError) as e:
+            Rectangle(3, 2, "x", 2)
+        self.assertEqual(str(e.exception), "x must be an integer")
+
+        with self.assertRaises(TypeError) as e:
+            Rectangle(2, 3, None, 2)
+        self.assertEqual(str(e.exception), "x must be an integer")
+
+    def test_invalid_y_type(self):
+        with self.assertRaises(TypeError) as e:
+            Rectangle(3, 2, 2, "y")
+        self.assertEqual(str(e.exception), "y must be an integer")
+
+        with self.assertRaises(TypeError) as e:
+            Rectangle(2, 3, 2, None)
+        self.assertEqual(str(e.exception), "y must be an integer")
+
+    def test_width_under_or_equal_zero(self):
+        with self.assertRaises(ValueError) as e:
+            Rectangle(0, 2)
+        self.assertEqual(str(e.exception), "Width must be > 0")
+
+        with self.assertRaises(ValueError) as e:
+            Rectangle(-5, 2)
+        self.assertEqual(str(e.exception), "Width must be > 0")
+
+    def test_height_under_or_equal_zero(self):
+        with self.assertRaises(ValueError) as e:
+            Rectangle(2, 0)
+        self.assertEqual(str(e.exception), "height must be > 0")
+
+        with self.assertRaises(ValueError) as e:
+            Rectangle(5, -2)
+        self.assertEqual(str(e.exception), "height must be > 0")
+
+    def test_x_under_zero(self):
+        with self.assertRaises(ValueError) as e:
+            Rectangle(3, 2, -2, 5)
+        self.assertEqual(str(e.exception), "x must be >= 0")
+
+    def test_y_under_zero(self):
+        with self.assertRaises(ValueError) as e:
+            Rectangle(3, 2, 2, -5)
+        self.assertEqual(str(e.exception), "y must be >= 0")
