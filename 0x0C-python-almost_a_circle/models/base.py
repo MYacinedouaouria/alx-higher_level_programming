133d10a5b501df34c971af9c85259f424c3e138a
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-12 09:24:26 -0800

    wrote tests for the Base class and documented Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module defines a class called 'Base'"""
+
+
+class Base:
+    """ defines class Base with:
+        private class attribute : __nb_objects initialized to 0
+        class constructor: def __init__(self, id=None):
+    """
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,21 @@
+""" this is  a test file to test  the Base class"""
+
+
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """ test   Base"""
+
+    def test_base(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
