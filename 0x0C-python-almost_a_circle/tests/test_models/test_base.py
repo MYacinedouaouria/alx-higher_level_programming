7b90d805b8df7d88b2e1edad69a7e73ef91bd383
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-15 04:58:13 -0800

    added tests for Base() , asign auto an id and assign auto an id +1 id previous exists

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,14 +8,10 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     """ test   Base"""
 
-    def test_base(self):
+    def test_base_asign_auto_id(self):
         b1 = Base()
-        b2 = Base()
-        b3 = Base()
         self.assertEqual(b1.id, 1)
+
+    def test_base_asign_auto_id_plus1(self):
+        b2 = Base()
         self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
-        b4 = Base(12)
-        self.assertEqual(b4.id, 12)
-        b5 = Base()
-        self.assertEqual(b5.id, 4)
