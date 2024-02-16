c0513a1e3549dea9685f7033d91ddc8224c800e8
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-16 15:38:42 -0800

    added a suit of tests that test the display functionality of a square

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -53,3 +53,36 @@ class TestPrintSquare_Andarea(unittest.TestCase):
         expected_output = "[Square] (89) 1/3 - 3\n"
         self.assertEqual(s.area(), 9)
         self.assertEqual(output, expected_output)
+
+
+class TestDisplay(unittest.TestCase):
+
+    def setUp(self):
+        self.captured_output = StringIO()
+        sys.stdout = self.captured_output
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+        self.captured_output.truncate(0)
+        self.captured_output.seek(0)
+
+    def test_display_with_1args(self):
+        s = Square(5)
+        s.display()
+        output = self.captured_output.getvalue()
+        expected_output = "#####\n#####\n#####\n#####\n#####\n"
+        self.assertEqual(output, expected_output)
+
+    def test_display_with_2args(self):
+        s = Square(2, 2)
+        s.display()
+        output = self.captured_output.getvalue()
+        expected_output = "  ##\n  ##\n"
+        self.assertEqual(output, expected_output)
+
+    def test_display_with_3args(self):
+        s = Square(3, 1, 3)
+        s.display()
+        output = self.captured_output.getvalue()
+        expected_output = "\n\n\n ###\n ###\n ###\n"
+        self.assertEqual(output, expected_output)
