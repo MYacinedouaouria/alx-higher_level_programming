e43cf25b870f63bc91354f3d3ee022a58ace48be
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-17 06:43:58 -0800

    wrote the method update that assigns attributes for a square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -38,3 +38,20 @@ class Square(Rectangle):
                 self.height = size
         else:
             raise TypeError("width must be an integer")
+
+    def update(self, *args, **kwargs):
+        """asigns attributes"""
+
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                if (hasattr(self, key)):
+                    setattr(self, key, value)
