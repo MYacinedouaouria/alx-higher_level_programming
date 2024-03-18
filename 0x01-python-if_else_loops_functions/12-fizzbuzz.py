e5c997f96f917ee0192bf0b5941b725a9d2b5851
MYacinedouaouria <yacine23i@hotmail.com>
2024-03-18 07:33:00 -0700

    Fizz Buzzwq

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def fizzbuzz():
+    """ FIZZ BUZZ"""
+
+    i = 1
+    while (i <= 100):
+        if ((i % 3 == 0) and (i % 5 == 0)):
+            print("FizzBuzz", end="")
+            print(" ", end="")
+            i += 1
+        elif (i % 3 == 0):
+            print("Fizz ", end="")
+            i += 1
+        elif (i % 5 == 0):
+            print("Buzz ", end="")
+            i += 1
+        else:
+            print(i, end=" ")
+            i += 1
