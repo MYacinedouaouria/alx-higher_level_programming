49ad19a7e7a4001c1804c46e900f59e0eb4a8639
MYacinedouaouria <yacine23i@hotmail.com>
2024-04-14 13:22:45 -0700

    solving errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,30 @@
-#!/usr/bin/python3
-"""this script lists all states with a name starting with N (upper N)
-    from the database hbtn_0e_0_usa"""
+!/usr/bin/python3
+"""This script lists all states with a name starting with 'N'
+    from the database hbtn_0e_0_usa."""
 
-
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    if (len(sys.argv) == 4):
-        try:
-            conn = MySQLdb.connect(
-                    host="localhost",
-                    user=sys.argv[1],
-                    passwd=sys.argv[2],
-                    db=sys.argv[3],
-                    port=3306
-                    )
-            cur = conn.cursor()
-            cur.execute(
-                    "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-                    )
-            query_rows = cur.fetchall()
-            for row in query_rows:
-                print(row)
-            cur.close()
-            conn.close()
-        except MySQLdb.Error as err:
-            print(f"{err}")
+    try:
+        conn = MySQLdb.connect(
+                host="localhost",
+                ser=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
+                )
+        cur = conn.cursor()
+
+        cur.execute(
+                "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+                )
+        query_rows = cur.fetchall()
+
+        for row in query_rows:
+            print(row)
+
+        cur.close()
+        conn.close()
+
+    except MySQLdb.Error as err:
+        print(f"Error connecting to database: {err}")
