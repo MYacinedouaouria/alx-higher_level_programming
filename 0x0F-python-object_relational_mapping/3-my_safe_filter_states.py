a0e9eff713fc85841585ebbe482878406ec64311
MYacinedouaouria <yacine23i@hotmail.com>
2024-04-14 15:10:28 -0700

    wrote a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, wrote one that is safe from MySQL injections!

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""This script retrieves states from hbtn_0e_0_usa matching the provided
+state name.
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    if len(sys.argv) == 5:
+        try:
+            conn = MySQLdb.connect(
+                host="localhost",
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
+            )
+            cur = conn.cursor()
+
+            state_name = sys.argv[4]
+            query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+            cur.execute(query, (state_name,))
+
+            query_rows = cur.fetchall()
+
+            if not query_rows:
+                print("No state found")
+            else:
+                for row in query_rows:
+                    print(row)
+
+            cur.close()
+            conn.close()
+
+        except MySQLdb.Error as err:
+            print(f"Error connecting to database: {err}")
