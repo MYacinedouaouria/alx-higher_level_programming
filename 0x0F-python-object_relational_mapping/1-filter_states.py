73f20648580389d4c553d2fb24474fda8dd28f94
MYacinedouaouria <yacine23i@hotmail.com>
2024-04-14 13:28:39 -0700

    removing try except

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,25 +6,21 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    try:
-        conn = MySQLdb.connect(
-                host="localhost",
-                ser=sys.argv[1],
-                passwd=sys.argv[2],
-                db=sys.argv[3]
-                )
-        cur = conn.cursor()
+    conn = MySQLdb.connect(
+            host="localhost",
+            ser=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cur = conn.cursor()
 
-        cur.execute(
-                "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-                )
-        query_rows = cur.fetchall()
+    cur.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+            )
+    query_rows = cur.fetchall()
 
-        for row in query_rows:
-            print(row)
+    for row in query_rows:
+        print(row)
 
-        cur.close()
-        conn.close()
-
-    except MySQLdb.Error as err:
-        print(f"Error connecting to database: {err}")
+    cur.close()
+    conn.close()
