4a2344b89919b4d483fc223da630fe4608dbeedc
MYacinedouaouria <yacine23i@hotmail.com>
2024-04-14 12:47:57 -0700

    added the port number

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,8 @@ if (__name__ == "__main__"):
                     host="localhost",
                     user=sys.argv[1],
                     passwd=sys.argv[2],
-                    db=sys.argv[3]
+                    db=sys.argv[3],
+                    port=3306
                     )
             cur = conn.cursor()
             cur.execute("SELECT * FROM states ORDER BY id ASC")
