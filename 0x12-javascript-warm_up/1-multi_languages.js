46c67458478669e98557d5f0479699f851ff492f
yacine-pc <yacine23i@hotmail.com>
2024-04-22 20:35:36 +0100

    wrote a script that prints 3 line

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// this script prints 3 lines
+
+let str = 'C is fun';
+console.log(str);
+str = 'Python is cool';
+console.log(str);
+str = 'JavaScript is amazing';
+console.log(str);
